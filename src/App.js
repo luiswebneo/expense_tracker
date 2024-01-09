d367cbc1068b2c10e9fea7b70b58b31f3bebe12e
luiswebneo <luisfernandolopes0@gmail.com>
2024-01-09 19:43:21 -0300

    Começando o app finanças

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import * as C from './App.styles';
+
+const App = () =>{
+  return(
+    <C.Container>
+      <C.Header>
+        <C.HeaderText>Controle Finançeiro</C.HeaderText>
+      </C.Header>
+      <C.Body>
+        ...
+      </C.Body>
+    </C.Container>
+  );
+}
+
+export default App;
